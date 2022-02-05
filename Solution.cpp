cef986bbbdbb56b050c9ce854c76639a5dc5d237
Pr1nceping <99080472+Pr1nceping@users.noreply.github.com>
2022-02-06 02:22:03 +0800

    Add files via upload

--- Solution.cpp
+++ Solution.cpp
@@ -2,15 +2,11 @@
  * This file defined a sort of incomplete functions about linked list.
  * You need to complete these functions to make all the test cases passed.
  */
-
 #include "Solution.h"
-
-
-
 ListNode* Solution::BuildFromXToY(int X, int Y) {
     //TODO: Task 2 Please insert your code here
     ListNode* head;
-    ListNode* newnode;
+    ListNode* newnode=nullptr;
     ListNode* temp;
     if(Y>=X) {
         head = new ListNode;
@@ -18,7 +14,7 @@ ListNode* Solution::BuildFromXToY(int X, int Y) {
         head->next = nullptr;
         temp = head;
         if (X != Y) {
-            for (int i = X; i <= Y; i++) {
+            for (int i = X; i < Y; i++) {
                 newnode = new ListNode;
                 newnode->value = i + 1;
                 temp->next = newnode;
@@ -34,15 +30,40 @@ ListNode* Solution::BuildFromXToY(int X, int Y) {
 }
 int Solution::CountLength(ListNode *head) {
     //TODO: Task 3 Please insert your code here
-    return -1;
+    int length =0;
+    ListNode* current = head; //
+    while(current!=nullptr){
+        length = length + 1;
+        current=current->next;
+    }
+    return length;
 }
 
 ListNode* Solution::AddNode(ListNode *head, int newValue) {
     //TODO: Task 4 Please insert your code here
-    return head;
+    ListNode* newnode=nullptr;
+    ListNode* current = head;
+    ListNode* temp;
+    if(newValue<current->value){
+        newnode = new ListNode;
+        newnode->value=newValue;
+        newnode->next=current;
+        current = newnode;
+    }
+    else if(newValue>current->value){
+        for(int i = current->value;newValue>i;i++){
+            temp->value=i;
+            current=current->next;
+        }
+        newnode = new ListNode;
+        newnode->value=newValue;
+        newnode->next=current->next;
+        temp->next=newnode;
+    }
+    return current;
 }
 
 ListNode* Solution::DeleteNode(ListNode *head, int targetValue) {
     //TODO: Task 5 Please insert your code here
-    return head;
+
 }
